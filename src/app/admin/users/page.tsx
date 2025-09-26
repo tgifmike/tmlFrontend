ecfe6da3d3ba9b71a7a763b8a141730b982f8beb
Mike <mr.mikesorrentino@gmail.com>
2025-09-26 16:40:33 -0400

    init

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+      <main>
+          <h1>Users</h1>
+          
+
+          <div>
+              <Link href="/">Home</Link>
+          </div>
+          </main>
+  )
+}
+
+export default page
