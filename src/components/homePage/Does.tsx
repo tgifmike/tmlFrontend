9e07404e2166a59ad01a2caecb9a9c33528ac397
Mike <mr.mikesorrentino@gmail.com>
2026-08-14 11:18:12 -0400

    change screen shots

--- src/components/homePage/Does.tsx
+++ src/components/homePage/Does.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { CircleCheck } from 'lucide-react';
 import Lightbox from 'yet-another-react-lightbox';
-import 'yet-another-react-lightbox/styles.css';
+// import 'yet-another-react-lightbox/styles.css';
 import { motion, useInView } from 'framer-motion';
 import { useRef } from 'react';
 
